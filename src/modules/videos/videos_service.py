77661543ff1e16a0f80d36f22dd92ccff0cd4fb8
Ashik Sarkar <ashiksarkar.s18@gmail.com>
2025-11-26 01:47:52 +0600

    - add static videos

--- src/modules/videos/videos_service.py
+++ src/modules/videos/videos_service.py
@@ -1,9 +1,33 @@
 videos = [
   {
     "id": 1,
-    "videoUrl": "https://example.com/video.mp4",
-    "title": "Sample Title",
-    "tags": ["tag1", "tag2"]
+    "videoUrl": "https://cdn.pixabay.com/video/2025/10/31/313145_large.mp4",
+    "title": "Heaven Nature",
+    "tags": ["nature", "outdoor", "heaven"]
+  },
+  {
+    "id": 2,
+    "videoUrl": "https://cdn.pixabay.com/video/2025/10/05/308073_large.mp4",
+    "title": "Beautiful Bird",
+    "tags": ["bird", "nature", "wildlife"]
+  },
+  {
+    "id": 3,
+    "videoUrl": "https://cdn.pixabay.com/video/2025/11/07/314643_large.mp4",
+    "title": "Beach Sun",
+    "tags": ["beach", "sun", "summer"]
+  },
+  {
+    "id": 4,
+    "videoUrl": "https://cdn.pixabay.com/video/2025/09/24/306155_large.mp4",
+    "title": "Wavey Ocean",
+    "tags": ["ocean", "waves", "sea"]
+  },
+  {
+    "id": 5,
+    "videoUrl": "https://cdn.pixabay.com/video/2025/09/22/305657_large.mp4",
+    "title": "Colorful Forest",
+    "tags": ["forest", "colors", "nature"]
   }
 ]
 
